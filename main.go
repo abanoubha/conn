8317a346ff3398483e8ee3920d8c317b56581b98
Abanoub Hanna <abanoubhannaa73@gmail.com>
2022-01-07 06:48:26 +0200

    nmap works

--- main.go
+++ main.go
@@ -1,75 +1,69 @@
 package main
 
 import (
-	"bufio"
-	"os"
-	"os/exec"
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Ullaakut/nmap/v2"
 )
 
 func main() {
-	whichPath, lookErr := exec.LookPath("/usr/bin/which")
-	if lookErr != nil {
-		panic(lookErr)
+	ds := runNmap()
+
+	fmt.Printf("connected devices : %d \n", len(ds)-1)
+
+	for i, d := range ds {
+		fmt.Println(i, d)
 	}
-
-	nmapPath, err := exec.Command(whichPath, "nmap").CombinedOutput()
-	if err != nil {
-		panic(err.Error())
-	}
-	// os.Stderr.WriteString(string(nmapPath)) // exact as below
-	// fmt.Println(string(nmapPath))
-
-	nmapStr := string(nmapPath)
-	err = runNmap(nmapStr)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// output, err := exec.Command("nmap -sn 192.168.1.0/24").CombinedOutput()
-	// if err != nil {
-	// 	os.Stderr.WriteString(err.Error())
-	// }
-	// fmt.Println(string(output))
-
-	// env := os.Environ()
-	// execErr := syscall.Exec(string(nmapPath), args, env)
-	// if execErr != nil {
-	// 	panic(execErr)
-	// }
 }
 
-func runNmap(nmap string) error {
-	cmd := exec.Command(nmap, "-sn", "192.168.1.0/24")
-	cmdReader, err := cmd.StdoutPipe()
+func runNmap() []string {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	scanner, err := nmap.NewScanner(
+		nmap.WithCustomArguments("-sn", "192.168.1.0/24"),
+		nmap.WithContext(ctx),
+	)
 	if err != nil {
-		return err
+		log.Fatalf("unable to create nmap scanner: %v", err)
 	}
-	scanner := bufio.NewScanner(cmdReader)
-	done := make(chan bool)
-	go func() {
-		for scanner.Scan() {
-			os.Stdout.WriteString(scanner.Text())
+
+	result, warnings, err := scanner.Run()
+	if err != nil {
+		log.Fatalf("unable to run nmap scan: %v", err)
+	}
+
+	if warnings != nil {
+		log.Printf("Warnings: \n %v", warnings)
+	}
+
+	// Use the results to print an example output
+	for _, host := range result.Hosts {
+		if len(host.Ports) == 0 || len(host.Addresses) == 0 {
+			continue
 		}
-		done <- true
-	}()
-	cmd.Start()
-	<-done
 
-	err = cmd.Wait()
-	if err != nil {
-		return err
+		fmt.Printf("Host %q:\n", host.Addresses[0])
+
+		for _, port := range host.Ports {
+			fmt.Printf("\tPort %d/%s %s %s\n", port.ID, port.Protocol, port.State, port.Service.Name)
+		}
 	}
 
-	return nil
-}
+	//fmt.Printf("connected devices : %d \n", len(result.Hosts))
 
-// func runIt(nmap string) ([]byte, error) {
-// 	var out bytes.Buffer
-// 	cmd := exec.Command(nmap, "-sn", "192.168.1.0/24")
-// 	cmd.Stdout = &out
-// 	err := cmd.Run()
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return out.Bytes(), nil
-// }
+	// for _, host := range result.Hosts {
+	// 	fmt.Println(host.Addresses[0])
+	// }
+
+	devices := []string{}
+
+	for _, host := range result.Hosts {
+		devices = append(devices, host.Addresses[0].Addr)
+	}
+
+	return devices
+}
