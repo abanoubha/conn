b6baaeb396a6cdd3324372675d2da2645f9693a9
Abanoub Hanna <abanoubhannaa73@gmail.com>
2022-01-07 06:52:49 +0200

    nmap works

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/Ullaakut/nmap/v2"
+	"github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/gtk"
 )
 
 func main() {
@@ -17,6 +20,36 @@ func main() {
 	for i, d := range ds {
 		fmt.Println(i, d)
 	}
+}
+
+func ui() {
+	const appID = "com.abanoubhanna.conn"
+	application, err := gtk.ApplicationNew(appID, glib.APPLICATION_FLAGS_NONE)
+
+	if err != nil {
+		log.Fatal("Could not create application.", err)
+	}
+	// Application signals available
+	// startup -> sets up the application when it first starts
+	// activate -> shows the default first window of the application (like a new document). This corresponds to the application being launched by the desktop environment.
+	// open -> opens files and shows them in a new window. This corresponds to someone trying to open a document (or documents) using the application from the file browser, or similar.
+	// shutdown ->  performs shutdown tasks
+	// Setup Gtk Application callback signals
+	application.Connect("activate", func() { onActivate(application) })
+	// Run Gtk application
+	os.Exit(application.Run(os.Args))
+}
+
+func onActivate(application *gtk.Application) {
+	// Create ApplicationWindow
+	appWindow, err := gtk.ApplicationWindowNew(application)
+	if err != nil {
+		log.Fatal("Could not create application window.", err)
+	}
+	// Set ApplicationWindow Properties
+	appWindow.SetTitle("conn : list all connected devices")
+	appWindow.SetDefaultSize(700, 800)
+	appWindow.Show()
 }
 
 func runNmap() []string {
